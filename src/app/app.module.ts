98658d4cf19be0f33a8944f4309176c90e3f87b4
magnusjw <magnusjw@clarkson.edu>
2020-12-30 09:59:11 -0500

    Angular Structuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UserComponent } from './components/user/user.component';
+import { PokemonPageComponent } from './components/pokemon-page/pokemon-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    UserComponent,
+    PokemonPageComponent
   ],
   imports: [
     BrowserModule,
