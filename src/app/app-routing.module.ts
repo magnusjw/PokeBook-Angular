24e60c714ac4f432c070921e6c1d0e8f0a368985
magnusjw <magnusjw@clarkson.edu>
2021-01-19 17:21:49 -0500

    File Management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ import { ViewUserComponent } from './components/view-user/view-user.component';
 import { UserFeedComponent } from './components/user-feed/user-feed.component';
 import { RerouteComponent } from './components/reroute/reroute.component';
 
+//http://localhost:8080/pokemon/15
+
 const routes: Routes = [
   { path:'', component:LoginComponent },
   { path:'login', component:LoginComponent },
